a2c4e945c025dbc4301ec1774f267a28faaaac2e
David Majdandžić <dmajdandzic@neos.hr>
2023-06-17 01:17:56 +0200

    Fix workflow and header

--- apps/web/src/lib/components/headers/BaseHeader.svelte
+++ apps/web/src/lib/components/headers/BaseHeader.svelte
@@ -8,16 +8,16 @@
 </script>
 
 <div class="flex h-20 items-center justify-between px-4" data-tauri-drag-region>
-    <nav>
-        <ul class="flex gap-x-8 text-3xl font-bold [&_a:hover]:text-primary [&_a]:transition-colors">
-            <li>
-                <a href="/route1"> Route1 </a>
-            </li>
-            <li>
-                <a href="/route2"> Route2 </a>
-            </li>
-        </ul>
-    </nav>
+<!--    <nav>-->
+<!--        <ul class="flex gap-x-8 text-3xl font-bold [&_a:hover]:text-primary [&_a]:transition-colors">-->
+<!--            <li>-->
+<!--                <a href="/route1"> Route1 </a>-->
+<!--            </li>-->
+<!--            <li>-->
+<!--                <a href="/route2"> Route2 </a>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--    </nav>-->
 
     <div class="[&>*:hover]:text-primary flex items-center space-x-4 [&>*]:transition-all [&>*]:duration-300">
         <ThemeToggle/>
